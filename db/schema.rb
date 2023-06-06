ee7528f8716ca1fc2705b8d073fd3aaf8d023afc
Erik Peteri <peteri.erik@gmail.com>
2023-06-06 11:02:21 +0300

    exercise 5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_165903) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_064345) do
+  create_table "beers", force: :cascade do |t|
+    t.string "name"
+    t.string "style"
+    t.integer "brewery_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "breweries", force: :cascade do |t|
     t.string "name"
     t.integer "year"
