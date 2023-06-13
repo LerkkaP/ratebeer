1f5608341947b18d859955d9ad49662e22e815d5
Erik Peteri <peteri.erik@gmail.com>
2023-06-13 17:50:19 +0300

    week 2, exercise 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,9 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+      scores = ratings.map { |name| name.score}
+      (scores.sum / ratings.count).to_f
+    end
 end
