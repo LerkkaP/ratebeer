3d9165642cd74c328ac20c49841278416a0e7a2e
Erik Peteri <peteri.erik@gmail.com>
2023-06-20 11:50:23 +0300

    week 3, exercise 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user
+
+    def current_user
+      return nil if session[:user_id].nil?
+      user = User.find_by(id: session[:user_id])
+      return user if user.present?
+    
+      session[:user_id] = nil
+      nil
+    end    
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :breweries
   root 'breweries#index'
   get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
   #get 'kaikki_bisset', to: 'beers#index'
   #get 'ratings', to: 'ratings#index'
   #get 'ratings/new', to:'ratings#new'
