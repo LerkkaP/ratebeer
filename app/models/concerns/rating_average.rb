5fa43d8c2fb7c13cd93b3edae8c448d1fbcda827
Erik Peteri <peteri.erik@gmail.com>
2023-06-20 20:50:51 +0300

    week 3, exercise 5

--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -2,7 +2,11 @@ module RatingAverage
   extend ActiveSupport::Concern
 
   def average_rating
-    scores = ratings.map(&:score)
-    (scores.sum.to_f / ratings.count).truncate(1)
+    if ratings.empty?
+      0  # tai muu haluamasi oletusarvo
+    else
+      scores = ratings.map(&:score)
+      (scores.sum.to_f / ratings.count).truncate(1)
+    end
   end
 end
