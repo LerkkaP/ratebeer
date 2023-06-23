cd43d50b03286bf86a3b33539be5531c51a49ee5
Erik Peteri <peteri.erik@gmail.com>
2023-06-23 11:59:05 +0300

    week 3, exercise 14 and 15

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,9 @@ class User < ApplicationRecord
   validates :username, uniqueness: true,
                        length: { minimum: 3, maximum: 30 }
 
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :beer_clubs, through: :memberships
 
   def already_member_of?(beer_club)
