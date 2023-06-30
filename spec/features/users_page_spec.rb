6933ce0404899cb1f9b12e43a8eb87bf7672982c
Erik Peteri <peteri.erik@gmail.com>
2023-06-30 09:08:46 +0300

    week 4, exercise 9

--- spec/features/users_page_spec.rb
+++ spec/features/users_page_spec.rb
@@ -6,6 +6,7 @@ describe "User" do
   let!(:user) { FactoryBot.create(:user, username: "Pekka", password: "Foobar1") }
   let!(:brewery) { FactoryBot.create(:brewery, name: "Koff") }
   let!(:beer1) { FactoryBot.create(:beer, name: "iso 3", brewery: brewery) }
+  let!(:beer2) { FactoryBot.create(:beer, name: "Sandels", style: "Weizen", brewery: brewery) }
 
   describe "who has signed up" do
     before :each do
@@ -45,6 +46,18 @@ describe "User" do
 
       expect(current_path).to eq(user_path(user.id))
       expect(page).to have_content "iso 3 10"
+    end
+    
+    it "has favorite style and brewery shown" do
+      rating1 = FactoryBot.create(:rating, score: 10, beer: beer1, user: user)
+      rating2 = FactoryBot.create(:rating, score: 25, beer: beer2, user: user)
+
+      visit user_path(user)
+
+      expect(page).to have_content "iso 3 10"
+      expect(page).to have_content "Sandels 25"
+      expect(page).to have_content "Weizen"
+      expect(page).to have_content "Koff"
     end
 
     it "can delete their own rating and it is removed from database" do
