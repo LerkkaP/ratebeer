6202a32a695569fa43dd90076b8a9fe142b93d9b
Erik Peteri <peteri.erik@gmail.com>
2023-07-01 20:32:20 +0300

    week 4, exercise 16

--- spec/features/beers_page_spec.rb
+++ spec/features/beers_page_spec.rb
@@ -4,9 +4,10 @@ include Helpers
 
 describe "Beer" do
     let!(:brewery) { FactoryBot.create :brewery, name: "Koff" }
+    let!(:user) { FactoryBot.create :user, username: "Pekka", password: "Foobar1" }
 
     before :each do
-        sign_in(username: "Pekka", password: "Foobar1")
+        sign_in(username: user.username, password: user.password)
     end
 
     it "is added if name is not blank" do
