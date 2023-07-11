de756bcab00f62d5b152bffd23405043b4d1952b
Erik Peteri <peteri.erik@gmail.com>
2023-07-11 20:12:21 +0300

    still fixing rubocop

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -1,50 +1,49 @@
 class StylesController < ApplicationController
-    def index
-      @styles = Style.all
-    end
-  
-    def show
-      @style = Style.find(params[:id])
-      @beers = Beer.where(style_id: @style.id)
-    end
-  
-    def new
-      @style = Style.new
-    end
-  
-    def create
-      @style = Style.new(style_params)
-      if @style.save
-        redirect_to styles_path, notice: "Style created successfully."
-      else
-        render :new
-      end
-    end
-  
-    def edit
-      @style = Style.find(params[:id])
-    end
-  
-    def update
-      @style = Style.find(params[:id])
-      if @style.update(style_params)
-        redirect_to styles_path, notice: "Style updated successfully."
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-      @style = Style.find(params[:id])
-      @style.beers.destroy_all 
-      @style.destroy
-      redirect_to styles_path, notice: "Style deleted successfully."
-    end
-  
-    private
-  
-    def style_params
-      params.require(:style).permit(:name, :description)
+  def index
+    @styles = Style.all
+  end
+
+  def show
+    @style = Style.find(params[:id])
+    @beers = Beer.where(style_id: @style.id)
+  end
+
+  def new
+    @style = Style.new
+  end
+
+  def create
+    @style = Style.new(style_params)
+    if @style.save
+      redirect_to styles_path, notice: "Style created successfully."
+    else
+      render :new
     end
   end
-  
+
+  def edit
+    @style = Style.find(params[:id])
+  end
+
+  def update
+    @style = Style.find(params[:id])
+    if @style.update(style_params)
+      redirect_to styles_path, notice: "Style updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @style = Style.find(params[:id])
+    @style.beers.destroy_all
+    @style.destroy
+    redirect_to styles_path, notice: "Style deleted successfully."
+  end
+
+  private
+
+  def style_params
+    params.require(:style).permit(:name, :description)
+  end
+end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -7,7 +7,7 @@ class Beer < ApplicationRecord
   has_many :raters, -> { distinct }, through: :ratings, source: :user
 
   validates :name, presence: true
-  #validates :style, presence: true
+  # validates :style, presence: true
 
   def average
     return 0 if ratings.empty?
